9be45143f6a0837ee51f254bd6345acf86b0c08e
Caio32Pro <caio32823@gmail.com>
2024-09-30 15:03:35 -0300

    Commit final da atividade

--- src/main/java/com/example/ExLivraria/controller/LivroControl.java
+++ src/main/java/com/example/ExLivraria/controller/LivroControl.java
@@ -3,21 +3,46 @@ package com.example.ExLivraria.controller;
 import com.example.ExLivraria.model.Livros;
 import com.example.ExLivraria.repository.LivrosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/apiAluno")
+@RequestMapping(value = "/apiLivros")
 public class LivroControl {
 
     @Autowired
     LivrosRepository bookRepo;
 
-    @GetMapping(value = "todos")
+    @GetMapping(value = "/todos")
     public List<Livros> listaLivros(){
         return bookRepo.findAll();
     }
+
+    @GetMapping("/livros/{IdLivro}")
+    public Optional<Livros> buscarLivrosPorId(@PathVariable(value = "IdLivro") int IdLivro){
+        return bookRepo.findById(IdLivro);
+    }
+
+    @PostMapping("/cadastrarLivro")
+    public void culturaDeGraca(@RequestBody Livros book){
+        bookRepo.save(book);
+    }
+
+    @DeleteMapping("/excluirLivros")
+    public void censuraGratuita(@RequestBody Livros book){
+        bookRepo.delete(book);
+    }
+
+    @DeleteMapping("/excluirId/{IdLivro}")
+    public void censuraGratuitaPorId(@PathVariable(value = "IdLivro") int IdLivro){
+        bookRepo.deleteById(IdLivro);
+    }
+
+    @PutMapping("/atualizar")
+    public void atualizarLivros(@RequestBody Livros book){
+        bookRepo.save(book);
+    }
+
 }
--- src/main/java/com/example/ExLivraria/model/Livros.java
+++ src/main/java/com/example/ExLivraria/model/Livros.java
@@ -1,4 +1,44 @@
 package com.example.ExLivraria.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
 public class Livros {
+
+    @Id
+    private int IdLivro;
+
+    @Column
+    private String nome;
+
+    @Column
+    private double preco;
+
+    public int getIdLivro() {
+        return IdLivro;
+    }
+
+    public void setIdLivro(int idLivro) {
+        IdLivro = idLivro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
